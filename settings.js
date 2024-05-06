0c460ef5370c7548c481def0d95d4256b4efac68
Madushankasly <168985992+Madushankasly@users.noreply.github.com>
2024-05-06 16:08:02 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,11 +16,11 @@ Creator : @darkmakerofc , @mr-nima-x
 */
 
 module.exports = {
-  OWNER_NUMBER: "94767411875",
-  SUDO_NUMBERS : "94767411875",
+  OWNER_NUMBER: "94716787803",
+  SUDO_NUMBERS : "94716787803",
   OWNER_NAME: "『 𝙼𝚁 𝙽𝙸𝙼𝙰 』",
   BOT_NAME : "ＺＵＳＹＣＯ",
-  ZUSYCO_FILE : "ZUSYCO-QR.nima",
+  ZUSYCO_FILE : "ZUSYCO-QR-3.nima",
   LOGOS: {
     ALIVE: 'https://i.ibb.co/YTWznr9/ZUSYCO-ALIVE-IMAGE-LOWQ.jpg',
     WELCOME: 'https://i.ibb.co/PzhpBV7/WELCOME-IMAGE-LOWQ.jpg',
